5f1e7ad14e788eb20b12aa0a16736b5f8ac560f8
dimpusagar91 <dimpusagar91@gmail.com>
2017-02-17 16:07:04 +0530

    Practice Programs
    Practice Programs

---
+++ amstrong_number.py
@@ -0,0 +1,33 @@
+# Python program to check if the number provided by the user is an Armstrong number or not
+
+import sys
+
+# initialise sum
+sum = 0
+
+ 
+num = int(input("Enter a number: "))
+
+
+# find the sum of the cube of each digit
+
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
+
+
+ 
+
+
+
+
+
+
